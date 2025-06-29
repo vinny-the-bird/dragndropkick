743866b68fd1cddd8a71a1785c0340e4ee7a6350
Vincent <vincent.kyk.vogel@protonmail.com>
2025-06-29 20:59:04 +0200

    changed title (crazy hu)

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,31 @@
   <header>
     <div>
       <h1>Welcome to Drag'n'Dropkick</h1>
+      <h3>'Throwing Stuff Since 1999'</h3>
+    </div><br><br><br>
+
+        <div>
+Current Mood <br>
+<img src="\src\assets\tumblr_e50997d1ab05f24c6ebf2ac5c0a324ce_9fdd06aa_1280.png" alt="">
+<!-- <img src="\src\assets\tumblr_eae6411a640722124bac6f6b47a239bb_1bb0ab2e_1280.png" alt=""> -->
+<!-- <img src="\src\assets\tumblr_cafc63c3feee892f74947449bdc5d18b_4e9fdc86_1280.png" alt=""> -->
     </div>
-    <h3>'Throwing Stuff Since 1999'</h3>
+
   </header>
 
   <main>
-    <img
-      src="/src/assets/jamie_training.jpg"
-      alt="Young Jamie doing Drunken Style training."
-      width="100%"
-    />
+    <figure>
+      <img
+        src="/src/assets/jamie_training.jpg"
+        alt="Young Jamie doing Drunken Style training."
+        width="100%"
+      />
+    <figcaption><i>
+Original art from Jamie World Tour Story - Street Fighter 6 (Capcom)
+    </i></figcaption>
+    </figure>
+    
+
   </main>
 </template>
 
