14e82b88b404f59cc5e3997da30f085c6e02fbd0
Vincent-Jung <117348000+Vincent-Jung@users.noreply.github.com>
2025-08-14 11:29:40 +0200

    cleared code and made variable names more explicit. added cda deadline

--- src/views/TheLongestDay.vue
+++ src/views/TheLongestDay.vue
@@ -3,39 +3,34 @@
     <h1 class="title is-4">The Longest Day</h1>
 
     <div class="container">
-      <div class="container" style="padding: 2rem">
-        <h1 class="subtitle is-4">Today we are</h1>
-        <h1 class="title is-5">{{ date }}</h1>
-        <br />
-        <h1 class="subtitle is-4">Deadline CDA</h1>
+      <div class="box">
+        <h1 class="subtitle is-4">Today is</h1>
+        <h1 class="title is-5">{{ today }}</h1>
+      </div>
+      <div class="box">
+        <h1 class="subtitle is-4">Certification Deadline in</h1>
         <h1 class="title is-5">{{ remainingTimeCda }}</h1>
-        <br />
-        <h1 class="subtitle is-4">Time remaining</h1>
-        <h1 class="title is-5">{{ remainingtTimeFinal }}</h1>
+      </div>
+      <div class="box">
+        <h1 class="subtitle is-4">Final Exam in</h1>
+        <h1 class="title is-5">{{ remainingtTimeExam }}</h1>
       </div>
     </div>
   </div>
 </template>
+
 <script setup>
-// import { ref } from "vue";
-
-
-const date = new Date();
-console.log(date);
-
-
+// TODO: add tick every second = make it a dynamic clock
+const today = new Date();
 const currentTime = Date.now();
 
-const goalDate = new Date(2026, 4, 26);
+const deadlineExam = new Date(2026, 4, 26, 18);
+const deadlineCda = new Date(2025, 10, 3, 18);
 
-// const goalDateTime = goalDate.getTime();
-
-const deadlineCda = new Date(2025, 10, 3);
-
-const msFinal = goalDate - currentTime;
+const msExam = deadlineExam - currentTime;
 const msCda = deadlineCda - currentTime;
 
-const remainingtTimeFinal = formatRemainingTime(msFinal);
+const remainingtTimeExam = formatRemainingTime(msExam);
 const remainingTimeCda = formatRemainingTime(msCda);
 
 function formatRemainingTime(ms) {
@@ -46,14 +41,20 @@ function formatRemainingTime(ms) {
   const minutes = Math.floor((totalSeconds % 3600) / 60);
   const seconds = totalSeconds % 60;
 
-  const parts = [];
-  if (days) parts.push(`${days} day${days !== 1 ? "s" : ""}`);
-  if (hours) parts.push(`${hours} hour${hours !== 1 ? "s" : ""}`);
-  if (minutes) parts.push(`${minutes} minute${minutes !== 1 ? "s" : ""}`);
-  if (seconds) parts.push(`${seconds} second${seconds !== 1 ? "s" : ""}`);
+  const displayedDate = [];
+  if (days) displayedDate.push(`${days} day${days !== 1 ? "s" : ""}`);
+  if (hours) displayedDate.push(`${hours} hour${hours !== 1 ? "s" : ""}`);
+  if (minutes)
+    displayedDate.push(`${minutes} minute${minutes !== 1 ? "s" : ""}`);
+  if (seconds)
+    displayedDate.push(`${seconds} second${seconds !== 1 ? "s" : ""}`);
 
-  return parts.join(", ");
+  return displayedDate.join(", ");
 }
-
 </script>
-<style scoped></style>
+
+<style scoped>
+.container {
+  padding: 2rem;
+}
+</style>
