26e00563be31667db9230529c093a842f4d8e300
Vincent-Jung <117348000+Vincent-Jung@users.noreply.github.com>
2025-11-27 14:49:49 +0100

    added simple button with sonic ring sound

--- src/views/JetPemRadio.vue
+++ src/views/JetPemRadio.vue
@@ -1,6 +1,8 @@
 <template>
+
       <div class="container global-frame">
     <h1 class="title is-4">Jet Pem Radio</h1>
+    <button class="button" @click="playSonicRing">Insert Coin</button>
     <MusicPlayer />
     </div>
 
@@ -9,7 +11,11 @@
 <script setup>
 import MusicPlayer from '@/components/MusicPlayer.vue';
 
+var sonicRing = new Audio("/sounds/26f8b9_sonic_ring_sound_effect.mp3");
 
+function playSonicRing() {
+  sonicRing.play();
+}
 
 </script>
 
